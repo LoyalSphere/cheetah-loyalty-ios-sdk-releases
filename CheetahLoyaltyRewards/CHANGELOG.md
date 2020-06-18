b226377b4de85763fdae0f4991743c9e990f2123
Julius Vitug <julius.vitug@cheetahdigital.com>
2020-06-18 15:58:47 +0800

    Add CheetahLoyaltyRewards 1.0.0

---
+++ CheetahLoyaltyRewards/CHANGELOG.md
@@ -0,0 +1,20 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [Rewards-1.0.0] - 2020-06-17
+
+### Added
+- Reward and Redemption Detail View Controllers
+- Reward Responders:
+	- Certificate
+	- Challenge
+	- Generic
+	- Physical
+	- Metric Transfer
+
+[Rewards-1.0.0]: https://github.com/LoyalSphere/cheetah-loyalty-ios-sdk/milestone/25?closed=1
---
+++ CheetahLoyaltyRewards/CheetahLoyaltyRewards.xcframework/ios-x86_64-simulator/CheetahLoyaltyRewards.framework/Headers/CheetahLoyaltyRewards.h
@@ -0,0 +1,19 @@
+//
+//  CheetahLoyaltyRewards.h
+//  CheetahLoyaltyRewards
+//
+//  Created by Jessel Esquinas on 28/04/2020.
+//  Copyright © 2020 Cheetah Digital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CheetahLoyaltyRewards.
+FOUNDATION_EXPORT double CheetahLoyaltyRewardsVersionNumber;
+
+//! Project version string for CheetahLoyaltyRewards.
+FOUNDATION_EXPORT const unsigned char CheetahLoyaltyRewardsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheetahLoyaltyRewards/PublicHeader.h>
+
+
